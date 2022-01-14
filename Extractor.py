83ddc6ede1b2d3536825450e6481ca77c308481b
Bram Simons <b.a.simons@student.tudelft.nl>
2022-01-14 13:28:57 +0100

    First commit

---
+++ Extractor.py
@@ -0,0 +1,15 @@
+
+
+class Extractor:
+    """A class that gets in an image and uses that to find a """
+    def __init__(self, image):
+
+        # The image that gets "found"
+        self.m_image = image
+
+        pass
+
+    def process(self):
+
+        pass
+
